5a7393c66972cf09e756a58ee99256cec49841ad
hokole <hokole551@gmail.com>
2023-10-12 14:30:20 -0500

    Update wsgi.py

--- testproject/testproject/wsgi.py
+++ testproject/testproject/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+path = '/django-render-test-app/testproject/'
+if path not in sys.path:
+    sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testproject.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'testproject.settings'
+
 
 application = get_wsgi_application()
