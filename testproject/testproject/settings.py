deb6d0e0e691b242c80502e66e9bc6adedc89ba8
hokole <hokole551@gmail.com>
2023-10-12 17:12:13 -0500

    Fixed typo

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url 
 from pathlib import Path
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,11 +76,15 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 DATABASES['default'] = dj_database_url.parse('postgres://testdb_0pkf_user:dcvJsOccsmapYEDncLcS2U8qGKoWhObb@dpg-ckeu8fq0h1ls73fj78gg-a.oregon-postgres.render.com/testdb_0pkf')
